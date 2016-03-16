323549ae39fe7a9cbaf838ce377d3b66f63683ce
William Johnson <wjohnson1000@gmail.com>
2016-03-16 17:27:41 -0600

    added setup.py

--- process/getweather.py
+++ process/getweather.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("Users/Billy/Git/First_Chair_Server/process")
+print sys.path
 import processconfig
 import unirest
 response = unirest.get("http://api.wunderground.com/api/" + processconfig.api_key['wunderground'] + "/conditions/q/CO/Denver.json")
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='firstchair',
+      version='1.0',
+      py_modules=['process.processconfig'],
+      )
