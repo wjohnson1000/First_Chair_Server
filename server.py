8d70a08a0d8839c9cabe35d11cc9a6433cf044e5
William Johnson <wjohnson1000@gmail.com>
2016-03-16 20:16:06 -0600

    changing port

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import os
 #import unirest
-PORT = 'PORT'
 app = Flask(__name__)
 
 #gresponse = unirest.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=" + config.api_key['google'])
@@ -12,5 +11,5 @@ def hello():
     return "Hello World"
 
 if __name__ == "__main__":
-    port = int(os.environ.get('$PORT', 5000))
+    PORT = int(os.environ.get('$PORT', 5000))
     app.run(host='0.0.0.0', port=PORT)
