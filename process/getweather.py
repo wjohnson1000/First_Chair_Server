74740efb21b126f0f037bc8229c8283ba130e701
William Johnson <wjohnson1000@gmail.com>
2016-03-17 17:24:49 -0600

    restructured module file structure

--- process/getweather.py
+++ process/getweather.py
@@ -1,16 +1,11 @@
-import sys
-#sys.path.append("Users/Billy/Git/First_Chair_Server/process")
-#sys.path.append("Users/Billy/Git/First_Chair_Server/db")
-#print sys.path
-#import processconfig as api_config
+import modules.processconfig as api_config
+import modules.db_config as db_config
+#import process.processconfig as api_config
 #import db.db_config as db_config
-import process.processconfig as api_config
-import db.db_config as db_config
 import unirest
 import psycopg2
 from sqlalchemy import *
 
-#meta = MetaData()
 engine = create_engine(db_config.db_var['url'])
 connection = engine.connect()
 response = unirest.get("http://api.wunderground.com/api/" + api_config.api_key['wunderground'] + "/conditions/q/CO/Denver.json")
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='firstchair',
       version='1.0',
-      packages=['pkg'],
-      package_dir={'pkg': './'},
-      py_modules=['process.processconfig', 'db.db_config']
+      package_dir = {'modules': 'process/modules'}
+      packages=['modules'],
+      py_modules=['process.modules'],
       )
