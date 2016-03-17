d111876b4120c36dae549a05a94b74f06da4c20d
William Johnson <wjohnson1000@gmail.com>
2016-03-17 17:27:54 -0600

    reordering packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='firstchair',
       version='1.0',
-      package_dir = {'modules': 'process/modules'}
       packages=['modules'],
+      package_dir = {'modules': 'process/modules'}
       py_modules=['process.modules'],
-      )
+     )
