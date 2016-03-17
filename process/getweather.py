b220cd370451a445895ce3fb1b16dad98792c76d
William Johnson <wjohnson1000@gmail.com>
2016-03-17 14:46:46 -0600

    trying to dump process data into database

--- process/getweather.py
+++ process/getweather.py
@@ -1,6 +1,14 @@
 import sys
 sys.path.append("Users/Billy/Git/First_Chair_Server/process")
-import process.processconfig as config
+sys.path.append("Users/Billy/Git/First_Chair_Server/db")
+import psycopg2
+import db_config as db_config
+import process.processconfig as api_config
 import unirest
-response = unirest.get("http://api.wunderground.com/api/" + config.api_key['wunderground'] + "/conditions/q/CO/Denver.json")
+from sqlalchemy import *
+
+#meta = MetaData()
+engine = create_engine(db_config.db_var['url'])
+connection = engine.connect()
+response = unirest.get("http://api.wunderground.com/api/" + api_config.api_key['wunderground'] + "/conditions/q/CO/Denver.json")
 print response.body
