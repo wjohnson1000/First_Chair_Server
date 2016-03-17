08426121914c998fa1063b70a83d1bcec2dafae2
William Johnson <wjohnson1000@gmail.com>
2016-03-17 13:05:39 -0600

    schema set up locally

--- db/db_api.py
+++ db/db_api.py
@@ -3,10 +3,51 @@ import psycopg2
 sys.path.append("Users/Billy/Git/First_Chair_Server/db")
 
 from sqlalchemy import *
-engine = create_engine('postgresql://localhost/broncos')
+meta = MetaData()
+engine = create_engine('postgresql://localhost/firstchair')
 connection = engine.connect()
-result = connection.execute("select * from roster")
-for row in result:
-  print row.player_name
-connection.close()
-print "connection closed"
+user = Table("user", meta,
+  Column("id", Integer, primary_key=True),
+  Column("google_id", Integer),
+  Column("snowfall_alarm", Integer),
+  Column("travel_window", String(10)),
+  Column("place_id", Integer, ForeignKey("place.id"))
+)
+
+user_place = Table("user_place", meta,
+  Column("user_id", Integer, ForeignKey("user.id")),
+  Column("place_id", Integer, ForeignKey("place.id"))
+)
+
+place = Table("place", meta,
+  Column("id", Integer, primary_key=True),
+  Column("address", String),
+  Column("city", String),
+  Column("state", String(10))
+)
+
+snowfall = Table("snowfall", meta,
+  Column("id", Integer, primary_key=True),
+  Column("place_id", Integer, ForeignKey("place.id")),
+  Column("snowfall", Integer),
+  Column("time", DateTime, server_default=text('NOW()'))
+)
+
+travel_time = Table("travel_time", meta,
+  Column("id", Integer, primary_key=True),
+  Column("place_id", Integer, ForeignKey("place.id")),
+  Column("user_id", Integer, ForeignKey("user.id")),
+  Column("travel_time", Integer),
+  Column("time", DateTime, server_default=text('NOW()'))
+)
+
+place.create(engine, checkfirst=True)
+user.create(engine, checkfirst=True)
+user_place.create(engine, checkfirst=True)
+snowfall.create(engine, checkfirst=True)
+travel_time.create(engine, checkfirst=True)
+#connection.close()
+#print "connection closed"
+#result = connection.execute("select * from roster")
+#for row in result:
+#  print row.player_name
--- process/gettraveltime.py
+++ process/gettraveltime.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append("Users/Billy/Git/First_Chair_Server/process")
+import process.processconfig as config #for heroku
+#import processconfig as config #for local testing
 import unirest
-#gresponse = unirest.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=" + config.api_key['google'])
-#print response.body
+gresponse = unirest.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=" + config.api_key['google'])
+print gresponse.body
