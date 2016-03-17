37a8f77bb84f6176155196c49bcadd10cd8f7007
William Johnson <wjohnson1000@gmail.com>
2016-03-17 17:42:48 -0600

    not sure

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ setup(name='firstchair',
       version='1.0',
       packages=['modules'],
       package_dir = {'modules': 'process/modules'},
-      py_modules=['processconfig', 'db_config']
      )
