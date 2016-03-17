efd3033a3b9f02a8f0c1a56c31e2247b137dd0dd
William Johnson <wjohnson1000@gmail.com>
2016-03-17 17:34:52 -0600

    changed setup file again

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ setup(name='firstchair',
       version='1.0',
       packages=['modules'],
       package_dir = {'modules': 'process/modules'},
+      py_modules=['processconfig', 'db_config']
      )
