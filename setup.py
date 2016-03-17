0b6e82a3466ad750fb95eff4717f5f9b942b08dd
William Johnson <wjohnson1000@gmail.com>
2016-03-17 17:30:40 -0600

    removed modules from setup file

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from distutils.core import setup
 setup(name='firstchair',
       version='1.0',
       packages=['modules'],
-      package_dir = {'modules': 'process/modules'}
-      py_modules=['process.modules'],
+      package_dir = {'modules': 'process/modules'},
      )
