f77ddaecf8077946839898f2fdfb1a54a8a0446a
William Johnson <wjohnson1000@gmail.com>
2016-03-16 20:30:54 -0600

    removed port

--- server.py
+++ server.py
@@ -11,5 +11,5 @@ def hello():
     return "Hello World"
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get('$PORT', 5000))
-    app.run(host='0.0.0.0', port=PORT)
+#    PORT = int(os.environ.get('$PORT', 5000))
+    app.run(host='0.0.0.0')
