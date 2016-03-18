25af86d59b6a8e554aa289937b936ad2392dacb3
William Johnson <wjohnson1000@gmail.com>
2016-03-17 22:28:33 -0600

    initial commit on branch, trying to use trx

--- process/getweather.py
+++ process/getweather.py
@@ -6,11 +6,18 @@ import unirest
 import os
 import psycopg2
 from sqlalchemy import *
-
+from sqlalchemy.orm import *
 weather_key = os.environ.get("WUNDERGROUND_API_KEY")
-db_url = os.environ.get("DATABASE_URL")
-
-engine = create_engine(db_url)
+#db_url = os.environ.get("DATABASE_URL")
+db_url = "postgres://localhost/firstchair"
+engine = create_engine(db_url, echo=True)
 connection = engine.connect()
-response = unirest.get("http://api.wunderground.com/api/" + weather_key + "/conditions/q/CO/Denver.json")
-print response.body
+Session = sessionmaker(bind=engine)
+session = Session()
+#response = unirest.get("http://api.wunderground.com/api/" + weather_key + "/conditions/q/CO/Denver.json")
+#print response.body
+
+#USE SESSION.COMMIT() FOR MASS TRX?
+
+for instance in session.query(place):
+  print instance
