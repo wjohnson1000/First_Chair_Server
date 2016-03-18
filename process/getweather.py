5006471a9f9bc1494ed5cae3bbe80fef5b373940
William Johnson <wjohnson1000@gmail.com>
2016-03-17 21:09:05 -0600

    moved env vars

--- process/getweather.py
+++ process/getweather.py
@@ -1,12 +1,16 @@
-import processconfig as api_config
-import db_config as db_config
+#import processconfig as api_config
+#import db_config as db_config
 #import process.processconfig as api_config
 #import db.db_config as db_config
 import unirest
+import os
 import psycopg2
 from sqlalchemy import *
 
-engine = create_engine(db_config.db_var['url'])
+weather_key = os.environ.get("WUNDERGROUND_API_KEY")
+db_url = os.environ.get("DATABASE_URL")
+
+engine = create_engine(db_url)
 connection = engine.connect()
-response = unirest.get("http://api.wunderground.com/api/" + api_config.api_key['wunderground'] + "/conditions/q/CO/Denver.json")
+response = unirest.get("http://api.wunderground.com/api/" + weather_key + "/conditions/q/CO/Denver.json")
 print response.body
