7a2df92c25d390303003a4205ad3a6f46d4488cc
William Johnson <wjohnson1000@gmail.com>
2016-03-19 17:59:50 -0600

    oauth working on server side

--- server.py
+++ server.py
@@ -1,45 +1,51 @@
-from flask import Flask
+from flask import *
 import os
 import json
 import httplib2
-from apiclient import *
-from oauth2client import client
+import clientconfig
+from apiclient import discovery
+from oauth2client import client as client
 
 app = Flask(__name__)
 
+#for DEPLOYMENT
+#flow = OAuth2WebServerFlow(client_id='your_client_id',
+#                           client_secret='your_client_secret',
+#                           scope='https://www.googleapis.com/auth/calendar',
+#                           redirect_uri='http://example.com/auth_return')
+
+
+
 @app.route("/")
 def index():
-  if 'credentials' not in flask.session:
-    return flask.redirect(flask.url_for('callback'))
-  credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+  if 'credentials' not in session:
+    return redirect(url_for('oauth2callback'))
+  credentials = client.OAuth2Credentials.from_json(session['credentials'])
   if credentials.access_token_expired:
-    return flask.redirect(flask.url_for('callback'))
+    return redirect(flask.url_for('oauth2callback'))
   else:
-    http_auth = credentials.authorize(httplib2.Http())
-    drive_service = discovery.build('drive', 'v2', http_auth)
-    files = drive_service.files().list().execute()
-    return json.dumps(files)
+    return "authorized"
 
 
 @app.route('/callback')
 def oauth2callback():
-  flow = client.flow_from_clientsecrets(
-      'client_secrets.json',
-      scope='https://www.googleapis.com/auth/drive.metadata.readonly',
-      redirect_uri=flask.url_for('oauth2callback', _external=True),
-      include_granted_scopes=True)
-  if 'code' not in flask.request.args:
+  flow = client.flow_from_clientsecrets('client_secrets.json',
+    scope='https://www.googleapis.com/auth/plus.login',
+    redirect_uri=url_for('oauth2callback', _external=True))
+  if 'code' not in request.args:
     auth_uri = flow.step1_get_authorize_url()
-    return flask.redirect(auth_uri)
+    return redirect(auth_uri)
   else:
-    auth_code = flask.request.args.get('code')
+    auth_code = request.args.get('code')
     credentials = flow.step2_exchange(auth_code)
-    flask.session['credentials'] = credentials.to_json()
-    return flask.redirect(flask.url_for('index'))
+    session['credentials'] = credentials.to_json()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
   PORT = int(os.environ.get("PORT", 5000))
   import uuid
   app.secret_key = str(uuid.uuid4())
-  app.run(host='0.0.0.0', port=PORT)
+  app.run(port=PORT, debug=True)
+  #0.0.0.0 FOR HEROKU
+  #app.run(host='0.0.0.0', port=PORT)
 
