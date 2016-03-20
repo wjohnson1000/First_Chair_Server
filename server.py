32fd9dd0e40b354436c421f8b28db1a218a6a7d3
William Johnson <wjohnson1000@gmail.com>
2016-03-20 12:57:45 -0600

    moving write json task into server

--- server.py
+++ server.py
@@ -7,6 +7,21 @@ import httplib2
 from apiclient import discovery
 from oauth2client import client as client
 
+client_info = {
+  "web": {
+    "client_id": os.environ.get("client_id"),
+    "Project_id": os.environ.get("Project_id"),
+    "auth_uri": os.environ.get("auth_uri"),
+    "token_uri": os.environ.get("token_uri"),
+    "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+    "client_secret": os.environ.get("client_secret"),
+    "redirect_uris":[os.environ.get("redirect_uris")]
+  }
+}
+
+with open('secrets_from_env.json', 'w') as f:
+  json.dump(client_info, f)
+
 app = Flask(__name__)
 
 #for DEPLOYMENT
