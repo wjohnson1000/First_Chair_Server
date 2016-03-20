2cd0b39abed28ff2bc09d19175972378a3f2600e
William Johnson <wjohnson1000@gmail.com>
2016-03-20 17:33:25 -0600

    sending token in header

--- server.py
+++ server.py
@@ -49,7 +49,10 @@ def oauth2callback():
     token = jwt.encode({'auth_code': auth_code}, 'secret', algorithm='HS256')
     credentials = flow.step2_exchange(auth_code)
     session['credentials'] = credentials.to_json()
-    return redirect('http://127.0.0.1:8080/dashboard&token=' + token)
+    resp = flask.Response("hello")
+    resp.headers['token'] = token
+    return resp
+    #return redirect('http://127.0.0.1:8080/dashboard&token=' + token)
 
 if __name__ == "__main__":
   PORT = int(os.environ.get("PORT", 5000))
