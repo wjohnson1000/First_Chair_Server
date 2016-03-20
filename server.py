34253e9d6987482bd36b884ecd0ae75226378589
William Johnson <wjohnson1000@gmail.com>
2016-03-20 15:05:06 -0600

    send that token!

--- server.py
+++ server.py
@@ -49,7 +49,7 @@ def oauth2callback():
     token = jwt.encode({'auth_code': auth_code}, 'secret', algorithm='HS256')
     credentials = flow.step2_exchange(auth_code)
     session['credentials'] = credentials.to_json()
-    return redirect('http://127.0.0.1:8080/')
+    return redirect('http://127.0.0.1:8080/landing&token=' + token)
 
 if __name__ == "__main__":
   PORT = int(os.environ.get("PORT", 5000))
