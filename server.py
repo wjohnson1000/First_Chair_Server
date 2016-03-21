97db8103d3140ef09618f057beb4c86aa88b1a84
William Johnson <wjohnson1000@gmail.com>
2016-03-21 09:30:29 -0600

    back to it

--- server.py
+++ server.py
@@ -108,8 +108,8 @@ def routeInfo():
 def forecast():
   this_place = sesh.query(place).filter(place.city == "Vail").one()
   forecast = unirest.get("http://api.wunderground.com/api/" + weather_key + "/forecast/q/" + this_place.state + "/" + this_place.city + ".json")
-  print forecast.body.forecast
-  return jsonify({'isSnow': forecast.body.forecast})
+  print forecast.body['forecast']
+  return jsonify({'isSnow': forecast.body['forecast']})
 
 if __name__ == "__main__":
   PORT = int(os.environ.get("PORT", 5000))
