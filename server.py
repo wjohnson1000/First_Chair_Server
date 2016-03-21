43e8fd4fe05fb380f44942ea4cb46212b25a9333
William Johnson <wjohnson1000@gmail.com>
2016-03-21 14:49:50 -0600

    put it in the query string

--- server.py
+++ server.py
@@ -95,7 +95,7 @@ def oauth2callback():
     credentials = flow.step2_exchange(auth_code)
     session['credentials'] = credentials.to_json()
     #return resp
-    return redirect('http://127.0.0.1:8080/dashboard&token=' + token)
+    return redirect('http://127.0.0.1:8080/dashboard?token=' + token)
 
 @app.route("/route")
 def routeInfo():
