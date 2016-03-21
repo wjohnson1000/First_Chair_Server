ffa1d0466136598dde45e5060017bd074ef6f695
William Johnson <wjohnson1000@gmail.com>
2016-03-20 23:20:50 -0600

    debug time

--- server.py
+++ server.py
@@ -107,5 +107,5 @@ if __name__ == "__main__":
   app.secret_key = str(uuid.uuid4())
   #app.run(port=PORT, debug=True)
   #0.0.0.0 FOR HEROKU
-  app.run(host='0.0.0.0', port=PORT)
+  app.run(host='0.0.0.0', port=PORT, debug=True)
 
